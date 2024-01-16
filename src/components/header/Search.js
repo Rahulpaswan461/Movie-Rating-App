0dcf86bd5fbba067002496b3082a03d8f69c1631
Anup Bhaumik <141262471+anupBH77@users.noreply.github.com>
2024-01-16 19:02:13 +0530

    Update Search.js

--- src/components/header/Search.js
+++ src/components/header/Search.js
@@ -1,13 +1,37 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import Card from "../card/Card"
-
+import {useParams} from "react-router-dom"
 const Search = ({ searchMovie }) => {
-    console.log("API Response:", searchMovie);
+  const {key} = useParams();
+  const [searchData,setSearchData] = useState([]);
+
+  function fetchData (){
+    console.log(key)
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=4e44d9029b1270a757cddc766a1bcb63&query=${key}`)
+      .then(res => {
+        if (!res.ok) {
+          throw new Error(`HTTP error! Status: ${res.status}`);
+        }
+        return res.json();
+      })
+      .then(result => {
+        console.log("API Result:", result);
+        console.log("THe header component")
+        setSearchData(result.results);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }
+  useEffect(()=>{
+    fetchData();
+  },[key])
+
     return (
       <div className="movie__list">
         <div className="list__cards">
-          {searchMovie && searchMovie.length > 0 ? (
-            searchMovie.map((movie) => (
+          {searchData && searchData.length > 0 ? (
+            searchData.map((movie) => (
               <Card movie={movie} key={movie.id} />
             ))
           ) : (
